28b8968b87c6634d115030c9314dca992988cd56
muskan89500 <muskan80965@gmail.com>
2026-07-27 19:09:25 +0530

    Change admin password from 'admin123' to 'tannu'

--- app.py
+++ app.py
@@ -1007,7 +1007,7 @@ def page_admin():
         st.info("This page is restricted. Enter the admin password to continue.")
         pwd = st.text_input("Admin Password", type="password")
         if st.button("🔓 Login"):
-            expected = st.secrets.get("ADMIN_PASSWORD", "admin123")
+            expected = st.secrets.get("ADMIN_PASSWORD", "tannu")
             if pwd == expected:
                 st.session_state.admin_authenticated = True
                 st.rerun()
